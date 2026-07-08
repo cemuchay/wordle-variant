30cb7fdbcec3cee53af7f91d59bd10efdeecd67e
Chizalam <cemuchay@gmail.com>
2026-07-08 07:24:24 +0100

    fix: ununsed prop in guess-preview that caused build failure

--- src/components/guess-preview/index.tsx
+++ src/components/guess-preview/index.tsx
@@ -564,7 +564,6 @@ const GuessPreviewModal: React.FC<GuessPreviewModalProps> = ({
               targetWordToUse={targetWordToUse}
               challenge={challenge}
               marathonGameIndex={marathonGameIndex}
-              entry={entry}
               myParticipation={myParticipation}
             />
 
